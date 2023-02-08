7c51281795250ca53410d4068949c4ae66c3437e
pipi-shortstocking <jannie435@naver.com>
2023-02-09 00:42:44 +0900

    백준 10807

---
+++ 백준/10807/1차.py
@@ -0,0 +1,12 @@
+count = 0
+
+# 입력할 숫자의 개수
+n = int(input())
+
+# n값 만큼 숫자 입력하기 - 파이썬에서는 n만큼 입력받는 조건을 만족시키지 않아도 됨
+arr = list(map(int, input().split()))
+
+# 찾을 숫자 입력
+v = int(input())
+
+print(arr.count(v))
