3ba122d29ff5af5a5ec7e2fce4ccb357704dc2bd
pipi-shortstocking <jannie435@naver.com>
2023-02-27 16:37:22 +0900

    백준

--- 백준/10812/1차.py
+++ 백준/10812/1차.py
@@ -1,4 +1,17 @@
 n, m = map(int, input().split())
-arr = [i+1 for i in range(m)]
+arr = [i+1 for i in range(n)]
 
-print(arr)
+for a in range(m):
+    i, j, k = map(int, input().split())
+    m_e = arr[k-1:j]  # mid부터 end까지
+    # print(m_e)
+    z_b = arr[0:i-1]  # 0부터 begin까지
+    # print(z_b)
+    e_n = arr[j:n]  # end부터 n까지
+    # print(e_n)
+    b_m = arr[i-1:k-1]  # begin부터 mid까지
+    # print(b_m)
+    arr = z_b+m_e+b_m+e_n
+
+for b in range(len(arr)):
+    print(arr[b], end=' ')
