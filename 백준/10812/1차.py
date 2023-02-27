289c8c233530dcf72a6b4ab1912a2eabfb6322df
pipi-shortstocking <jannie435@naver.com>
2023-02-27 16:39:39 +0900

    주석 수정

--- 백준/10812/1차.py
+++ 백준/10812/1차.py
@@ -5,11 +5,11 @@ for a in range(m):
     i, j, k = map(int, input().split())
     m_e = arr[k-1:j]  # mid부터 end까지
     # print(m_e)
-    z_b = arr[0:i-1]  # 0부터 begin까지
+    z_b = arr[0:i-1]  # 0부터 begin 전 까지
     # print(z_b)
-    e_n = arr[j:n]  # end부터 n까지
+    e_n = arr[j:n]  # end 후부터 n까지
     # print(e_n)
-    b_m = arr[i-1:k-1]  # begin부터 mid까지
+    b_m = arr[i-1:k-1]  # begin부터 mid 전까지
     # print(b_m)
     arr = z_b+m_e+b_m+e_n
 
