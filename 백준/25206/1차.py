b4a4e5cc5f098c9c97071dad007f4597307ce230
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2023-03-08 23:46:39 +0900

    백준 25206

--- 백준/25206/1차.py
+++ 백준/25206/1차.py
@@ -1,13 +1,24 @@
-#grade = [[0]*3 for _ in range(3)]
-grade = [[0] for _ in range(3)]
+grade = [[0] for _ in range(20)]
 
-for i in range(3):
-    # sub, sco, gra = map(str, input().split())
-    # # grade[i].append(sub)
-    # grade[i] = sub
-    # grade[i].append(float(sco))
-    # grade[i].append(gra)
+standard = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0,
+            'C+': 2.5, 'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0, 'P': 0.0}
+grade_sum = 0
+score_sum = 0
+aveg = 0
 
+for i in range(20):
     grade[i] = list(map(str, input().split()))
 
-print(grade)
+    point = float(grade[i][1])
+    user_grade = standard[grade[i][2]]
+
+    if grade[i][2] != 'P' and 'F':
+        score_sum += point
+    grade_sum += point*user_grade
+
+aveg = grade_sum/score_sum
+
+# print(score_sum)
+# print(grade_sum)
+
+print(round(aveg, 6))
