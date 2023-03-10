215dc7cafcb4da5082c492bdd165b0abaf619ec7
pipi-shortstocking <jannie435@naver.com>
2023-03-10 13:56:47 +0900

    백준

---
+++ 백준/10798/1차.py
@@ -0,0 +1,23 @@
+arr = [[0]*5 for _ in range(5)]
+# word = [0]*5
+word = []
+length = [0]*5
+
+# print(arr)
+# print(word)
+
+for i in range(5):
+    arr[i] = str(input())
+    length[i] = len(arr[i])
+
+# print(length)
+# print(arr)
+
+for i in range(max(length)):
+    for j in range(5):
+        if i < length[j]:
+            word.append(arr[j][i])
+        # word[i] += arr[j][i]
+
+for i in word:
+    print(i, end='')
