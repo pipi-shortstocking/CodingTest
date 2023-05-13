60c4c972dc66d9140ffb5b96fe2c6f7edd719e07
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2023-05-14 04:00:21 +0900

    1018 미완성

--- 백준/1018/1차.py
+++ 백준/1018/1차.py
@@ -7,5 +7,10 @@ board = []
 for _ in range(n):
     board.append(input().rstrip())
 
-print(board[0])
-print(type(board[0])) # str
+# print(board[0])
+# print(type(board[0])) # str
+
+for i in range(n):
+    for j in range(m):
+        if (i+j) % 2 == 1: # 홀수
+            if board[]
