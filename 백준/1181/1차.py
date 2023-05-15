22886acb360fcd24402a3adc74ca00bc033f98ee
pipi-shortstocking <jannie435@naver.com>
2023-05-15 11:48:14 +0900

    1181 블로그

--- 백준/1181/1차.py
+++ 백준/1181/1차.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 
 arr = []
@@ -7,12 +8,12 @@ n = int(input())
 for _ in range(n):
     arr.append(str(input().rstrip()))
 
-arr.sort()
+arr.sort()  # 알파벳 순서
 
 for i in range(n):
     min_index = i
     min = len(arr[i])
-    for j in range(i+1, n):
+    for j in range(i + 1, n):
         if min > len(arr[j]):
             min = len(arr[j])
             min_index = j
