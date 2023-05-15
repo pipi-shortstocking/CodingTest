29a9a575938ceca31071fcf28de47506cf1357c7
pipi-shortstocking <jannie435@naver.com>
2023-05-15 10:37:37 +0900

    1018 ***

--- 백준/1018/1차.py
+++ 백준/1018/1차.py
@@ -1,8 +1,10 @@
 import sys
+
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 board = []
+count = []
 
 for _ in range(n):
     board.append(input().rstrip())
@@ -10,7 +12,22 @@ for _ in range(n):
 # print(board[0])
 # print(type(board[0])) # str
 
-for i in range(n):
-    for j in range(m):
-        if (i+j) % 2 == 1: # 홀수
-            if board[]
+for i in range(n - 7):
+    for j in range(m - 7):  # row, col 탐색 시, 제한을 정해줘야 배열 탈출 X -> 즉, 8x8 보드로 제한해줌
+        index1, index2 = 0, 0
+
+        for row in range(i, i + 8):
+            for col in range(j, j + 8):
+                if (row + col) % 2 == 0:  # 짝수
+                    if board[row][col] != "W":
+                        index1 += 1
+                    if board[row][col] != "B":
+                        index2 += 1
+                else:  # 홀수
+                    if board[row][col] != "B":
+                        index1 += 1
+                    if board[row][col] != "W":
+                        index2 += 1
+        count.append(min(index1, index2))
+
+print(min(count))
