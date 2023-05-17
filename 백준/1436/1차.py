2ea6c1e94ae28a0aac87feea880bc808c727d346
pipi-shortstocking <jannie435@naver.com>
2023-05-17 12:59:50 +0900

    1436

---
+++ 백준/1436/1차.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+num = 666
+
+while n != 0:
+    if "666" in str(num):
+        n -= 1
+        if n == 0:
+            break
+    num += 1
+
+print(num)
