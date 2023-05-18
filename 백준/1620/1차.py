762912e3e2ac881e1558a934d700955d7bcc87ba
pipi-shortstocking <jannie435@naver.com>
2023-05-18 19:05:00 +0900

    1620

--- 백준/1620/1차.py
+++ 백준/1620/1차.py
@@ -3,6 +3,18 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-list = {}
+list_num = {}
+list_name = {}
 
-for i in range(n):
+for i in range(1, n + 1):
+    name = str(input().rstrip())
+    list_num[i] = name
+    list_name[name] = i
+
+for j in range(m):
+    ans = input().rstrip()
+
+    if ans.isdigit():
+        print(list_num[int(ans)])
+    else:
+        print(list_name[ans])
