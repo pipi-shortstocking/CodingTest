60d80e126032c94fc860f2904bfcacbbada2886b
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2023-07-27 23:21:42 +0900

    1026

---
+++ 백준/1026/1차.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+s = 0
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(n):
+    s += a[i]*b[i]
+
+print(s)
