e9d812eaa8e71cead5af1eff80b2e96e09616176
pipi-shortstocking <jannie435@naver.com>
2023-08-03 17:23:02 +0900

    문자열 뒤집기

---
+++ 그리디/문자열 뒤집기/1차.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+s = str(input().rstrip())
+
+count0 = 0
+count1 = 0
+
+if s[0] == "1":
+    count0 += 1
+else:
+    count1 += 1
+
+for i in range(len(s) - 1):
+    if s[i] != s[i + 1]:
+        if s[i + 1] == "1":
+            count0 += 1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
