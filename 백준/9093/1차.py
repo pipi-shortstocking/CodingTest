a9ac1c6fbf113a9c1db3d2803a197f6afc85fdac
pipi-shortstocking <jannie435@naver.com>
2023-08-04 19:01:20 +0900

    9093

---
+++ 백준/9093/1차.py
@@ -0,0 +1,31 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    sen = str(input().rstrip())
+    answer = ""
+    start = 0
+    word = ""
+
+    # word += sen[::-1]
+    # print(word)
+
+    for i in range(len(sen)):
+        if sen[i] == " " or i == len(sen) - 1:  # 공백이거나 문장의 끝일 때
+            word = sen[start:i]
+            if i == len(sen) - 1:  # 문장 끝일 때에는 끝 인덱스 설정 X
+                word = sen[start:]
+
+            answer += word[::-1] + " "
+
+            word = ""
+            start = i + 1
+    answer = answer[: len(answer) - 1]
+    print(answer)
+    # print(list(answer))
+
+# word += str(t)
+# print(word)
