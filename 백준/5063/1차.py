83e118f03803f9b5971f711c7ad6a23d2a2664e3
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2023-12-12 22:53:59 +0900

    5063

---
+++ 백준/5063/1차.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    r,e,c = map(int, input().split())
+
+    if r < e-c:
+        print("advertise")
+    elif r == e-c:
+        print("does not matter")
+    elif r > e-c:
+        print("do not advertise")
