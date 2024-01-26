8070c7de0bf3997b7ee9ef2515327506f3e03f25
pipi-shortstocking <jannie435@naver.com>
2024-01-26 16:02:19 +0900

    1935 보완

--- 백준/1935/1차.py
+++ 백준/1935/1차.py
@@ -14,8 +14,11 @@ for i in range(n):
     num.append(int(input()))
 
 for i in range(len(cal)):
+    # if cal[i].isalpha(): 로 적어도 ok
+    # if 'A' <= cal[i] <= 'Z': 으로 적어도 ok
     if ord(cal[i]) >= 65 and ord(cal[i]) <= 90:  # 피연산자일 경우
         # cal[i] = num[ord(cal[i]) - 65]
+        # stack.append(num[ord(cal[i]) - ord('A')])로 적어도 ok
         stack.append(num[ord(cal[i]) - 65])
     else:  # 연산자일 경우
         b = stack.pop()
@@ -31,4 +34,5 @@ for i in range(len(cal)):
         elif cal[i] == "%":
             stack.append(a % b)
 
+# print("%.2f" %stack[0]) 으로 적어도 ok
 print("{:.2f}".format(stack[0]))
