4876ebc07c45d20338670f99d0c76c6d1680a8d4
pipi-shortstocking <jannie435@naver.com>
2024-02-14 23:56:28 +0900

    11005

--- 백준/11005/1차.py
+++ 백준/11005/1차.py
@@ -1,10 +1,10 @@
 n, b = map(int, input().split())
-rev_mod = ''
+rev_mod = ""
 
 while n > 0:
-    n, mod = divmod(n, b)
+    n, mod = divmod(n, b)  # n은 몫, mod는 나머지
     if mod >= 10:
-        mod = chr(mod+55)
+        mod = chr(mod + 55)
     rev_mod += str(mod)
 
 print(rev_mod[::-1])
