851d02c60336130fb04c6ab2335135c2d8c1b8e8
pipi-shortstocking <jannie435@naver.com>
2024-02-22 19:54:46 +0900

    15988

--- 백준/15988/3차.py
+++ 백준/15988/3차.py
@@ -7,6 +7,6 @@ dp = [1, 2, 4]
 
 for _ in range(t):
     n = int(input())
-    for i in range(len(dp), n):
+    for i in range(len(dp), n):  # len(dp)가 n보다 클 경우 해당 반복문 작업 X
         dp.append((dp[-3] + dp[-2] + dp[-1]) % 1000000009)
     print(dp[n - 1])
