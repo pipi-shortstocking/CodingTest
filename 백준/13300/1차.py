36948f39592688ae6cf559e1dd906967ee5c3e91
JeongYun Kim <95032287+pipi-shortstocking@users.noreply.github.com>
2024-03-27 00:34:44 +0900

    13300

--- 백준/13300/1차.py
+++ 백준/13300/1차.py
@@ -4,9 +4,22 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 stu = [[0, 0] for _ in range(6)]
+room = 12
 
 for _ in range(n):
     s, y = map(int, input().split())
     stu[y - 1][s] += 1
 
-print(stu)
+for i in range(6):
+    for j in range(2):
+        temp = stu[i][j]
+        if temp > k:
+            room -= 1
+            if temp % k == 0:
+                room += temp // k
+            else:
+                room += temp // k + 1
+        elif temp == 0:
+            room -= 1
+
+print(room)
