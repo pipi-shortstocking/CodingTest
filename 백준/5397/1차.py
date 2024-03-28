c5a757c9da36cf2c70cfbc164c9fd73c0d5c651d
pipi-shortstocking <jannie435@naver.com>
2024-03-28 15:12:14 +0900

    5397

---
+++ 백준/5397/1차.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())  # 테스트 개수
+
+for _ in range(n):
+    string = str(input().rstrip())
+    arr1 = []
+    arr2 = []
+
+    for ch in string:
+        if ch == "<" and arr1:
+            arr2.append(arr1.pop())
+        elif ch == ">" and arr2:
+            arr1.append(arr2.pop())
+        elif ch == "-" and arr1:
+            arr1.pop()
+        elif ch.isalpha() or ch.isdigit():  # 대소문자, 숫자
+            arr1.append(ch)
+
+    ans = arr1 + arr2
+    print("".join(ans))
