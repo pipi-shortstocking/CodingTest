0030873d8d32f20cddd780ccf171660d5065466f
pipi-shortstocking <jannie435@naver.com>
2024-04-16 16:54:31 +0900

    1697

---
+++ 백준/1697/1차.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+queue = deque()
+via = [-1 for _ in range(k + 1)]
+
+via[n] = 0
+queue.append(n)
+while queue:
+    x = queue.popleft()
+
+    for i in [x - 1, x + 1, x * 2]:
+        nx = i
+        if 0 <= nx < k + 1 and via[nx] == -1:
+            queue.append(nx)
+            via[nx] = via[x] + 1
+
+    if x == k:
+        break
+
+print(via[k])
