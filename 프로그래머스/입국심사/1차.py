dc1e9835bc58e139fe786efc0e49646c0f499f8b
pipi-shortstocking <jannie435@naver.com>
2026-02-01 17:49:11 +0900

    입국심사

---
+++ 프로그래머스/입국심사/1차.py
@@ -0,0 +1,28 @@
+def solution(n, times):
+    times.sort()
+    
+    def can_process(T):
+        total = 0
+
+        for time in times:
+            total += T // time
+
+        return total >= n # True는 시간을 더 줄일 수 있음, False는 시간을 더 늘려야 함
+    
+    start = 0
+    end = times[-1] * n
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if can_process(mid):
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return start
+
+n = 6
+times = [7, 10]
+
+print(solution(n, times))
