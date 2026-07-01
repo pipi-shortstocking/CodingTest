9dea8e20fbe44e530933adf2af4891ef7816cf63
pipi-shortstocking <jannie435@naver.com>
2026-07-01 15:28:32 +0900

    DFS template

---
+++ 알고리즘 모음/DFS.py
@@ -0,0 +1,23 @@
+## 재귀형
+def dfs_recursion(graph, node, visited):
+    # 현재 노드 방문 처리
+    visited[node] = True
+
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[node]:
+        if not visited[i]:
+            dfs_recursion(graph, i, visited)
+
+## 스택형
+def dfs_stack(start):
+    visited = [False] * (N + 1) # 방문 여부 리스트
+    stack = [start] # 시작 노드를 스택에 넣기
+
+    while stack:
+        v = stack.pop()
+
+        if not visited[v]:
+            visited[v] = True # 방문 처리
+
+            for u in graph[v]:
+                stack.append(u)
