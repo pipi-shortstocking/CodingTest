54176cde4d2b4ac6ce679e196d8b09c5873b8204
pipi-shortstocking <jannie435@naver.com>
2026-07-15 17:25:14 +0900

    징검다리

---
+++ 프로그래머스/징검다리/1차.py
@@ -0,0 +1,33 @@
+def solution(distance, rocks, n):
+    rocks.sort()
+    rocks.append(distance)
+    left, right = 0, rocks[-1]
+
+    while left <= right:
+        mid = (left + right) // 2
+        current = 0
+        cnt = 0
+
+        for rock in rocks:
+            dist = rock - current
+
+            if dist < mid:
+                cnt += 1
+
+                if cnt > n:
+                    break
+            else:
+                current = rock
+
+        if cnt > n: # n 이상으로 바위를 제거했으면 간격 줄이기
+            right = mid - 1
+        else:
+            answer = left
+            left = mid + 1
+    
+    return answer
+
+distance = 25
+rocks = [2, 14, 11, 21, 17]
+n = 2
+print(solution(distance, rocks, n))
