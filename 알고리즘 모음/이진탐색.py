9e9e8231c2f7a46986623dd7f6c55b99dd967167
pipi-shortstocking <jannie435@naver.com>
2026-07-15 16:22:09 +0900

    이진탐색

--- 알고리즘 모음/이진탐색.py
+++ 알고리즘 모음/이진탐색.py
@@ -1,3 +1,4 @@
+# 배열에서 특정 값을 찾을 때
 arr = []
 target = 0
 
@@ -13,3 +14,12 @@ def binary():
             left = mid + 1
         else:
             right = mid - 1
+
+# 조건을 만족하는 최솟값/최댓값
+# 최솟값일 경우
+if 조건 만족:
+    right = mid - 1 # 더 줄여보기
+else:
+    left = mid + 1
+
+return left
