f830362bfd3f6b893e33be138bb9f445b512abb6
pipi-shortstocking <jannie435@naver.com>
2026-08-05 18:29:26 +0900

    가장 큰 정사각형 찾기

---
+++ 프로그래머스/가장 큰 정사각형 찾기/2차.py
@@ -0,0 +1,24 @@
+def solution(board):
+    n = len(board)
+    m = len(board[0])
+    dp = [[0] * m for _ in range(n)]
+    max_dp = 0
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 0:
+                dp[i][j] = 0
+            elif i < 1 or j < 1:
+                dp[i][j] = board[i][j]
+            else:
+                dp[i][j] = min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j]) + 1
+
+            if dp[i][j] > max_dp:
+                max_dp = dp[i][j]
+
+    return max_dp * max_dp
+
+board = [[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]
+# board = [[0,0,1,1],[1,1,1,1]]
+# board = [[1]]
+print(solution(board))
